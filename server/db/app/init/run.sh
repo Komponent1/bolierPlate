d570a4bb98ef53b147a374177a14eac038b81ade
seo2im <seo2im6492@gmail.com>
2022-11-11 23:53:01 +0900

    ✨ 서버 플레이트 제작

---
+++ server/db/app/init/run.sh
@@ -0,0 +1,1 @@
+psql -U $DB_ID -d $DB_NAME -a -f /sql_script/init.sql
---
+++ server/db/app/sql_script/init.sql
@@ -0,0 +1,13 @@
+/**
+* WRTIE YOUR SQL SCRIPT!
+*
+* GRANT ALL PRIVILEGES ON DATABASE test TO seo2im;
+* \c test;
+* CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+* 
+* CREATE TABLE users(
+*   id        VARCHAR(100)  NOT NULL  PRIMARY KEY,
+*   src       TEXT          NOT NULL,
+*   github    VARCHAR(100)  NOT NULL
+* );
+*/
